5f1dda45d99d8da5bf41684d413b34226b87435d
Sohail Mohammed <sohailm498@gmail.com>
2016-11-09 18:16:07 -0600

    Login and registration using firebase database

--- app/src/main/java/ca/uoit/msohail/charityapp/MainActivity.java
+++ app/src/main/java/ca/uoit/msohail/charityapp/MainActivity.java
@@ -1,19 +1,36 @@
 package ca.uoit.msohail.charityapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginEmail;
+    EditText loginPassword;
+    EditText loginUsername;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginEmail = (EditText)findViewById(R.id.edt_loginEmail);
+        loginUsername = (EditText)findViewById(R.id.edt_loginUsername);
+        loginPassword = (EditText)findViewById(R.id.edt_loginPassword);
     }
 
     public void openRegisterPage(View view){
@@ -22,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginVerify(View view){
-        EditText userLabel = (EditText)findViewById(R.id.lbl_userLogin);
+    /*    EditText userLabel = (EditText)findViewById(R.id.lbl_userLogin);
         EditText passLabel = (EditText)findViewById(R.id.lbl_passLogin);
 
         String userCheck = userLabel.getText().toString();
@@ -40,7 +57,24 @@ public class MainActivity extends AppCompatActivity {
         else {
             verifyLogin.makeText(this, "Username or password incorrect, try again", verifyLogin.LENGTH_LONG).show();
         }
+        */
+        progressDialog = ProgressDialog.show(MainActivity.this, "Please wait...", "Processing...", true);
 
+        (firebaseAuth.signInWithEmailAndPassword(loginEmail.getText().toString(), loginPassword.getText().toString()))
+                .addOnCompleteListener(new OnCompleteListener <AuthResult>(){
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            progressDialog.dismiss();
+
+                            Toast.makeText(MainActivity.this, "Sucessfully registered", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Login incorrect, try again", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
 
 
     }
--- app/src/main/java/ca/uoit/msohail/charityapp/Register.java
+++ app/src/main/java/ca/uoit/msohail/charityapp/Register.java
@@ -1,27 +1,107 @@
 package ca.uoit.msohail.charityapp;
 
 import android.app.Activity;
+import android.app.Application;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by 100485582 on 10/10/2016.
  */
 
-public class Register  extends Activity{
+public class Register  extends Activity {
+
+    EditText registerUsername;
+    EditText registerPassword;
+    EditText registerEmail;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+        registerUsername = (EditText)findViewById(R.id.registerUsername);
+        registerPassword = (EditText)findViewById(R.id.registerPassword);
+        registerEmail = (EditText)findViewById(R.id.registerEmail);
+
+
+
+
     }
 
 
     public void storeUserInfo(View view){
 
-        finish();
+
+        FirebaseAuth firebaseAuth;
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        String username = registerUsername.getText().toString();
+        String password = registerPassword.getText().toString();
+        String email = registerEmail.getText().toString();
+
+        Toast.makeText(this, username, Toast.LENGTH_LONG).show();
+
+        Log.i("CharityApp: ", username);
+        if(TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Need to enter username", Toast.LENGTH_LONG).show();
+
+        }
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Need to enter email", Toast.LENGTH_LONG).show();
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Need to enter email", Toast.LENGTH_LONG).show();
+        }
+        else {
+            progressDialog = ProgressDialog.show(Register.this, "Please wait...", "Processing...", true);
+           /* progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Please wait, the user is being registered");
+            progressDialog.show();*/
+
+            (firebaseAuth.createUserWithEmailAndPassword(email, password))
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(Register.this, "Sucessfully registered", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(Register.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(Register.this, "Did not sucessfully register", Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+        /* */
+
+        }
+        //finish();
 
     }
-
 
 }
