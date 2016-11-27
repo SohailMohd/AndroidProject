ad38202a9e5b9800fb2048196746c1c717f71942
Sohail Mohammed <sohailm498@gmail.com>
2016-11-26 23:48:33 -0600

    Used Google places Api to enable search for places in Nearby Charity activity, made some changes to style of app, also switched the json parsing data into a Australia Charities activity

---
+++ app/src/main/java/ca/uoit/msohail/charityapp/NearbyCharities.java
@@ -0,0 +1,8 @@
+package ca.uoit.msohail.charityapp;
+
+/**
+ * Created by 100485582 on 11/19/2016.
+ */
+
+public class NearbyCharities {
+}
---
+++ app/src/main/java/ca/uoit/msohail/charityapp/NearbyCharity_ArrayAdapter.java
@@ -0,0 +1,8 @@
+package ca.uoit.msohail.charityapp;
+
+/**
+ * Created by 100485582 on 11/20/2016.
+ */
+
+public class NearbyCharity_ArrayAdapter {
+}
